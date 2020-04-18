c436758d34082f5b0c0c35ae4ba364356796e903
kumarstack55 <kumarstack55@gmail.com>
2020-04-19 06:28:36 +0900

    Add scripts for exception handling

---
+++ try-catch/01_remove-item.ps1
@@ -0,0 +1,22 @@
+﻿try {
+    Remove-Item C:\nonexistent\file.txt -ErrorAction Stop
+} catch [System.Management.Automation.ErrorRecord] {
+    "実行されない。PowerShellの言語仕様は変だと思う。"
+} catch [System.Management.Automation.ItemNotFoundException] {
+    "実行されない。PowerShellの言語仕様は変だと思う。"
+} catch [System.Management.Automation.ActionPreferenceStopException] {
+    # -ErrorAction で、エラー時、 ActionPreferenceStopException が投げられる
+    # ActionPreferenceStopException オブジェクトを得る手段はわからない
+    if ($_.Exception -is [System.Management.Automation.ItemNotFoundException]) {
+        # catch句で、例外にアクセスするには $_ を使う。
+        # このオブジェクトは、 ErrorRecord である。
+        $_.GetType().FullName
+            # --> System.Management.Automation.ErrorRecord
+
+        # より具体的な例外は Exception プロパティが持つ。
+        $_.Exception.GetType().FullName
+            # --> System.Management.Automation.ItemNotFoundException
+    } else {
+        Throw $_
+    }
+}
---
+++ try-catch/02_div-by-zero.ps1
@@ -0,0 +1,10 @@
+﻿try {
+    1/0
+} catch [System.Management.Automation.ErrorRecord] {
+    "実行されない。PowerShellの言語仕様は変だと思う。"
+} catch [System.Management.Automation.ActionPreferenceStopException] {
+    "実行されない。ゼロ除算は -ErrorAction Stop とは別の例外であるため。"
+} catch [System.Management.Automation.RuntimeException] {
+    $_.GetType().FullName
+        # --> System.Management.Automation.RuntimeException
+}
