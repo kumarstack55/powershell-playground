9998bfdd519c41592bc36d74ca2b5e86294036f1
kumarstack55 <kumarstack55@gmail.com>
2020-04-19 14:57:33 +0900

    Add test-command.ps1

---
+++ try-catch/test-command.ps1
@@ -0,0 +1,18 @@
+﻿Function Test-CommandExists {
+    Param($Name)
+
+    try {
+        Get-Command -Name $Name -ErrorAction Stop | Out-Null
+        return $True
+    } catch [System.Management.Automation.ActionPreferenceStopException] {
+        $e = $_.Exception
+        if ($e -is [System.Management.Automation.CommandNotFoundException]) {
+            return $False
+        }
+        Throw $_
+    }
+}
+
+Test-CommandExists nvim
+Test-CommandExists notexists
+#$_.Exception.GetType().FullName
